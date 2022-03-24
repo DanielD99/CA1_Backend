e1e9ecdb7a6090a3c532096959a99a08d926d302
Alexander <Alexanderhusig1@gmail.com>
2022-03-24 18:48:17 +0100

    address, cityinfo + hobby dto

---
+++ src/main/java/dtos/AddressDTO.java
@@ -0,0 +1,63 @@
+package dtos;
+
+import entities.Address;
+import entities.CityInfo;
+import entities.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressDTO {
+
+    private int id;
+    private String street;
+    private String info;
+    private CityInfoDTO cityInfo;
+
+    public AddressDTO(Address address) {
+        this.id = address.getId();
+        this.street = address.getStreet();
+        this.info = address.getadditionalinfo();
+        this.cityInfo = new CityInfoDTO(address.getCityInfo());
+    }
+    public static List<AddressDTO> getDtos(List<Address> addressList){
+        List<AddressDTO> addressDTOS = new ArrayList();
+        addressList.forEach(address->addressDTOS.add(new AddressDTO(address)));
+        return addressDTOS;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    public CityInfoDTO getCityInfo() {
+        return cityInfo;
+    }
+
+    public void setCityInfo(CityInfoDTO cityInfo) {
+        this.cityInfo = cityInfo;
+    }
+}
+
+
+
