3a0ebcf19da3db71977911a83b46dff062b9db05
DanielD99 <71074562+DanielD99@users.noreply.github.com>
2022-03-24 19:24:50 +0100

    changes to PhoneDTO & PersonDTO

--- src/main/java/dtos/PhoneDTO.java
+++ src/main/java/dtos/PhoneDTO.java
@@ -1,67 +1,81 @@
+package dtos;
+
 ///*
 // * To change this license header, choose License Headers in Project Properties.
 // * To change this template file, choose Tools | Templates
 // * and open the template in the editor.
 // */
-//package dtos;
-//
-//import entities.RenameMe;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-///**
-// *
-// * @author tha
-// */
-//public class PhoneDTO {
-//    private long id;
-//    private String PhoneNumber;
-//    private String str2;
-//
-//    public PhoneDTO(String dummyStr1, String dummyStr2) {
-//        this.str1 = dummyStr1;
-//        this.str2 = dummyStr2;
-//    }
-//
-//    public static List<PhoneDTO> getDtos(List<RenameMe> rms){
-//        List<PhoneDTO> rmdtos = new ArrayList();
-//        rms.forEach(rm->rmdtos.add(new PhoneDTO(rm)));
-//        return rmdtos;
-//    }
-//
-//
+
+import entities.Phone;
+import entities.RenameMe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PhoneDTO {
+    private long id;
+    private String phoneNumber;
+    private String description;
+    private int personId;
+
+    public PhoneDTO(Phone phone) {
+        this.id = phone.getId();
+        this.phoneNumber = phone.getNumber();
+        this.description = phone.getDescription();
+        this.personId = phone.getPerson().getId();
+    }
+
+
+    public static List<PhoneDTO> getDtos(List<Phone> phoneList){
+        List<PhoneDTO> phoneDTOS = new ArrayList();
+        phoneList.forEach(phone->phoneDTOS.add(new PhoneDTO(phone)));
+        return phoneDTOS;
+    }
+
+
 //    public PhoneDTO(RenameMe rm) {
 //        if(rm.getId() != null)
 //            this.id = rm.getId();
 //        this.str1 = rm.getDummyStr1();
 //        this.str2 = rm.getDummyStr2();
 //    }
-//
-//    public String getDummyStr1() {
-//        return str1;
-//    }
-//
-//    public void setDummyStr1(String dummyStr1) {
-//        this.str1 = dummyStr1;
-//    }
-//
-//    public String getDummyStr2() {
-//        return str2;
-//    }
-//
-//    public void setDummyStr2(String dummyStr2) {
-//        this.str2 = dummyStr2;
-//    }
-//
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(int personId) {
+        this.personId = personId;
+    }
+
 //    @Override
 //    public String toString() {
 //        return "RenameMeDTO{" + "id=" + id + ", str1=" + str1 + ", str2=" + str2 + '}';
 //    }
-//
-//
-//
-//
-//
-//
-//}
+
+
+}
